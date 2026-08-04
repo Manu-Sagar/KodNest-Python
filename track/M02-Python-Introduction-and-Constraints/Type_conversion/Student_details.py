924bf1dfcfb1ff354e2ee6195b53ec102cb87029
MANU SAGAR <manusagar030@gmail.com>
2026-08-04 15:50:29 +0530

    Student Details

---
+++ track/M02-Python-Introduction-and-Constraints/Type_conversion/Student_details.py
@@ -0,0 +1,11 @@
+Student_name=input()
+Student_age=int(input())
+course_rating=float(input())
+
+print(f"Name:{Student_name}")
+print(f"Age:{Student_age}")
+print(f"Course Rating:{course_rating}")
+
+print(type(Student_name))
+print(type(Student_age))
+print(type(course_rating))
