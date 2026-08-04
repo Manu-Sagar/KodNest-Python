21ab6c1e3a310a4f23c4e6ad55095527cd4049cc
MANU SAGAR <manusagar030@gmail.com>
2026-08-04 15:18:57 +0530

    Topic-2

---
+++ track/M02-Python-Introduction-and-Constraints/Topic-1/Type_conversion.py
@@ -0,0 +1,11 @@
+age_text="21"
+course_fee_text="499.50"
+attempts=3
+
+age=int(age_text)
+course_fee_text=float(course_fee_text)
+attempts_text=str(attempts)
+
+print(age,type(age))
+print(course_fee_text,type(course_fee_text))
+print(attempts_text,type(attempts_text))
