5b0543cf8fff4f6c09c902cf182bb850c91610a4
MANU SAGAR <manusagar030@gmail.com>
2026-08-05 16:59:20 +0530

    Numbers_And_Charcters

---
+++ track/M02-Python-Introduction-and-Constraints/5-8-2026/For_Example.py
@@ -0,0 +1,10 @@
+N=int(input("Enter a number: "))
+Word=input("Enter a word: ")
+
+print("Numbers:")
+for i in range(1, N+1):
+    print(i)
+
+print("Characters:")
+for char in Word:
+    print(char)
