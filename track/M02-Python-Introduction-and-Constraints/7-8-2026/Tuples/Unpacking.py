8f51705d0e7b7fd48fb32a2862e3e67fa694c5a6
MANU SAGAR <manusagar030@gmail.com>
2026-08-07 15:50:24 +0530

    UnPacking Tuples

---
+++ track/M02-Python-Introduction-and-Constraints/7-8-2026/Tuples/Unpacking.py
@@ -0,0 +1,8 @@
+name=input("Enter the name:")
+course=input("Enter the Course:")
+score=int(input("Enter the score:"))
+student_record=(name,course,score)
+Name,Course,Score=student_record
+print("Name:",Name)
+print("Course:",Course)
+print("Score:",Score)
