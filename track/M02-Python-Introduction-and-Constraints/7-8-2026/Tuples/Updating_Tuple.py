55745ccd5b6588a9244a3f11e511422f15383505
MANU SAGAR <manusagar030@gmail.com>
2026-08-07 16:11:37 +0530

    Updating Tuple Values

---
+++ track/M02-Python-Introduction-and-Constraints/7-8-2026/Tuples/Updating_Tuple.py
@@ -0,0 +1,13 @@
+course_name=input()
+current_week=input()
+course_status=input()
+
+course_details=(course_name,current_week,course_status)
+
+updated_week=input()
+
+Updated_details=(course_name,updated_week,course_status)
+
+course_details=Updated_details
+
+print("Course Details:",course_details)
