46884d08c3bb2f132492c9fa372ee876acdd8a9a
MANU SAGAR <manusagar030@gmail.com>
2026-08-12 10:54:08 +0530

    Extracting Data Using Slicing

---
+++ track/M02-Python-Introduction-and-Constraints/Slicing/Extract_data.py
@@ -0,0 +1,11 @@
+word=input("Enter One Word:")
+first=int(input())
+second=int(input())
+third=int(input())
+
+numbers=[first,second,third]
+record=(first,second,third)
+
+print(f"Middle: {word[1:-1]}")
+print(f"First Two: {numbers[:2]}")
+print(f"Reversed Tuple:{record[::-1]}")
